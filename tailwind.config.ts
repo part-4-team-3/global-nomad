a6e109f9e59c63c36f6616b954f410c8846709e7
daeun <qzsz797979@gmail.com>
2024-07-04 11:31:02 +0900

    feat: color 변수 작성

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,12 @@ const range = (start: number, end: number) => {
 
 const config = {
   darkMode: ["class"],
-  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
     spacing: {
@@ -33,58 +38,27 @@ const config = {
     },
     extend: {
       colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+        "var-black": "#1B1B1B",
+        "var-primary": "#333236",
+        "var-gray1": "#4B4B4B",
+        "var-gray2": "#79747E",
+        "var-gray3": "#A4A1AA",
+        "var-gray4": "#ADAEB8",
+        "var-gray5": "#CBC9CF",
+        "var-gray6": "#DDDDDD",
+        "var-gray7": "#EEEEEE",
+        "var-gray8": "#FAFAFA",
+        "var-green": "#00AC07",
+        "var-green-dark": "#0B3B2D",
+        "var-green-light": "#F1EFFD",
+        "var-red-dark": "#FF472E",
+        "var-red-light": "#FFE4E0",
+        "var-orange-dark": "#FF7C1D",
+        "var-orange": "#FFF4E8",
+        "var-yellow": "#FFC23D",
+        "var-blue": "#0085FF",
+        "var-blue-dark": "#2EB4FF",
+        "var-blue-light": "#E5F3FF",
       },
     },
   },
