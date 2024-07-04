80a755c1e5110aa295bbc21adc3bfdfec37f2dd9
hakyoung12 <gkr097@naver.com>
2024-07-04 21:16:31 +0900

    feat: 이미지 생성 배열 생성

--- src/components/molecules/GeneratedImage/GeneratedImage.tsx
+++ src/components/molecules/GeneratedImage/GeneratedImage.tsx
@@ -5,13 +5,11 @@ interface Props {
 }
 export default function GeneratedImage({ url }: Props) {
   return (
-    <div className="relative inline-block">
+    <div className="relative rounded-3xl max-xl:h-[206px] max-xl:w-[206px] max-sm:h-[167px] max-sm:w-[167px] xl:h-[180px] xl:w-[180px]">
       {url && (
         <>
           <CloseButton />
-          <div className="relative rounded-3xl max-xl:h-[206px] max-xl:w-[206px] max-sm:h-[167px] max-sm:w-[167px] xl:h-[180px] xl:w-[180px]">
-            <Image fill src={url} alt="이미지 생성" />
-          </div>
+          <Image fill src={url} alt="이미지 생성" />
         </>
       )}
     </div>
--- src/components/molecules/ImageUploader/ImageUploader.tsx
+++ src/components/molecules/ImageUploader/ImageUploader.tsx
@@ -10,7 +10,7 @@ interface ApiResponse {
 }
 
 export default function ImageUploader() {
-  const [uploadedImage, setUploadedImage] = useState<string | undefined>();
+  const [uploadedImages, setUploadedImages] = useState<string[]>([]);
   /** 등록한 이미지를 url로 바꾸는 비동기 함수 */
   const handleUploadImage = async (e: React.ChangeEvent<HTMLInputElement>) => {
     /* input 요소에서 선택된 파일 추출 */
@@ -27,17 +27,23 @@ export default function ImageUploader() {
       const response: ApiResponse = await apiInstance.post('activities/image', formData, {
         headers: {
           /*임의로 발급한 엑세스토큰입니다. 추후 회원가입 기능 추가시 수정 예정 */
-          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTQ0LCJ0ZWFtSWQiOiI1LTMiLCJpYXQiOjE3MjAwOTI2ODMsImV4cCI6MTcyMDA5NDQ4MywiaXNzIjoic3AtZ2xvYmFsbm9tYWQifQ.s9euLsvwi3MYQ-ukWA5JOzoT3KR8IySpJGNtFc275KE`,
+          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTQ0LCJ0ZWFtSWQiOiI1LTMiLCJpYXQiOjE3MjAwOTQ2MDcsImV4cCI6MTcyMDA5NjQwNywiaXNzIjoic3AtZ2xvYmFsbm9tYWQifQ.S0zlNmbHD9Sd34AWRWW3vVUTPxrbMBGcwcNS5b3ds5w`,
         },
       });
-      setUploadedImage(response.activityImageUrl);
+      // 업로드된 이미지 배열이 4개 이하인 경우에만 추가
+      if (uploadedImages.length < 4) {
+        setUploadedImages((prevImages) => [...prevImages, response.activityImageUrl]);
+      } else {
+        alert('이미지는 최대 4개까지만 업로드할 수 있습니다.');
+      }
     } catch (error) {
       alert(error);
     }
   };
 
   return (
-    <>
+    /* 고정값으로 설정한 width는 페이지 레이아웃시 수정 예정입니다. */
+    <div className="grid gap-[24px] max-xl:w-[428px] max-xl:grid-cols-2 max-sm:w-[342px] xl:w-[792px] xl:grid-cols-4">
       <label>
         <input
           type="file"
@@ -50,7 +56,9 @@ export default function ImageUploader() {
           <Image fill src="/UploadImage.svg" alt="이미지 등록하기" />
         </div>
       </label>
-      <GeneratedImage url={uploadedImage} />
-    </>
+      {uploadedImages.map((imageUrl, index) => (
+        <GeneratedImage key={index} url={imageUrl} />
+      ))}
+    </div>
   );
 }
