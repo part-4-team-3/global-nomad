1821ace688fa0e1bafcce60adae92764b4accb0a
striker1826 <alstjq123579@daum.net>
2024-07-04 21:32:08 +0900

    fix: tailwind css pxr 변환기 spacing 2400px 까지 적용되도록 구현 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const pxToRem = (px: number, base = 16) => `${px / base}rem`;
 
@@ -8,94 +8,94 @@ const range = (start: number, end: number) => {
 };
 
 const config = {
-  darkMode: ["class"],
+  darkMode: ['class'],
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
-  prefix: "",
+  prefix: '',
   theme: {
     spacing: {
-      ...range(1, 101).reduce<Record<string, string>>((acc, px) => {
+      ...range(1, 2401).reduce<Record<string, string>>((acc, px) => {
         acc[`${px}pxr`] = pxToRem(px);
         return acc;
       }, {}),
     },
     fontSize: {
-      ...range(12, 60).reduce<Record<string, string>>((acc, px) => {
+      ...range(12, 61).reduce<Record<string, string>>((acc, px) => {
         acc[`${px}pxr`] = pxToRem(px);
         return acc;
       }, {}),
     },
     container: {
       center: true,
-      padding: "2rem",
+      padding: '2rem',
       screens: {
-        "2xl": "1400px",
+        '2xl': '1400px',
       },
     },
     extend: {
       colors: {
-        "var-black": "#1B1B1B",
-        "var-primary": "#333236",
-        "var-gray1": "#4B4B4B",
-        "var-gray2": "#79747E",
-        "var-gray3": "#A4A1AA",
-        "var-gray4": "#ADAEB8",
-        "var-gray5": "#CBC9CF",
-        "var-gray6": "#DDDDDD",
-        "var-gray7": "#EEEEEE",
-        "var-gray8": "#FAFAFA",
-        "var-green": "#00AC07",
-        "var-green-dark": "#0B3B2D",
-        "var-green-light": "#F1EFFD",
-        "var-red-dark": "#FF472E",
-        "var-red-light": "#FFE4E0",
-        "var-orange-dark": "#FF7C1D",
-        "var-orange": "#FFF4E8",
-        "var-yellow": "#FFC23D",
-        "var-blue": "#0085FF",
-        "var-blue-dark": "#2EB4FF",
-        "var-blue-light": "#E5F3FF",
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
+        'var-black': '#1B1B1B',
+        'var-primary': '#333236',
+        'var-gray1': '#4B4B4B',
+        'var-gray2': '#79747E',
+        'var-gray3': '#A4A1AA',
+        'var-gray4': '#ADAEB8',
+        'var-gray5': '#CBC9CF',
+        'var-gray6': '#DDDDDD',
+        'var-gray7': '#EEEEEE',
+        'var-gray8': '#FAFAFA',
+        'var-green': '#00AC07',
+        'var-green-dark': '#0B3B2D',
+        'var-green-light': '#F1EFFD',
+        'var-red-dark': '#FF472E',
+        'var-red-light': '#FFE4E0',
+        'var-orange-dark': '#FF7C1D',
+        'var-orange': '#FFF4E8',
+        'var-yellow': '#FFC23D',
+        'var-blue': '#0085FF',
+        'var-blue-dark': '#2EB4FF',
+        'var-blue-light': '#E5F3FF',
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
         primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
         },
         secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
         },
         destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
         },
         muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
         },
         accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
         },
         popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
         },
         card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
         },
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require('tailwindcss-animate')],
 } satisfies Config;
 
 export default config;
