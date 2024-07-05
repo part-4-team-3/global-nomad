8ee0b67ff1c4fbd61cd6ccb59becab5db4e2e9bd
ohdong9795 <ohdong9795@naver.com>
2024-07-05 09:05:10 +0900

    feat: Add DateInput component, react-datepicker

---
+++ src/components/molecules/input/DateInput.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/image';
+import React, { forwardRef, useRef } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import styles from './DateInput.module.css';
+
+export interface DateInputProps {
+  onChange?: (date: Date | null) => void;
+  selected?: Date | null;
+}
+
+const DateInput = forwardRef<HTMLDivElement, DateInputProps>(({ onChange, selected }, ref) => {
+  const datePickerRef = useRef<DatePicker>(null);
+  return (
+    <div ref={ref} className="w-130pxr relative h-56pxr sm:w-full sm:max-w-[379px]">
+      <DatePicker
+        ref={datePickerRef}
+        selected={selected}
+        onChange={onChange}
+        dateFormat="yy/MM/dd"
+        placeholderText="YY/MM/DD"
+        className="h-56pxr w-full cursor-pointer rounded-md border border-var-gray2 px-20pxr py-16pxr"
+        wrapperClassName={styles.reactDatepickerWrapper}
+      />
+      <Image
+        className="absolute right-8pxr top-1/2 -translate-y-1/2 transform cursor-pointer"
+        src="/calendar.png"
+        alt="Calendar Icon"
+        width={32}
+        height={32}
+        onClick={() => {
+          if (datePickerRef.current) datePickerRef.current.setOpen(true);
+        }}
+      />
+    </div>
+  );
+});
+
+DateInput.displayName = 'DateInput';
+
+export default DateInput;
