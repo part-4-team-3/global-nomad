65e45346925ba844617a412c914dd01ff7b62264
hakyoung12 <gkr097@naver.com>
2024-07-05 10:31:37 +0900

    test: 이미지 렌더링 임시함수 구현

--- src/components/molecules/ImageUploader/ImageUploader.tsx
+++ src/components/molecules/ImageUploader/ImageUploader.tsx
@@ -23,23 +23,27 @@ export default function ImageUploader() {
     const formData = new FormData();
     formData.append('image', uploadFile);
 
-    try {
-      const response: ApiResponse = await apiInstance.post('activities/image', formData, {
-        headers: {
-          /*임의로 발급한 엑세스토큰입니다. 추후 회원가입 기능 추가시 수정 예정 */
-          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTQ0LCJ0ZWFtSWQiOiI1LTMiLCJpYXQiOjE3MjAwOTQ2MDcsImV4cCI6MTcyMDA5NjQwNywiaXNzIjoic3AtZ2xvYmFsbm9tYWQifQ.S0zlNmbHD9Sd34AWRWW3vVUTPxrbMBGcwcNS5b3ds5w`,
-        },
-      });
-      // 업로드된 이미지 배열이 4개 이하인 경우에만 추가
-      if (uploadedImages.length < 4) {
-        setUploadedImages((prevImages) => [...prevImages, response.activityImageUrl]);
-      } else {
-        alert('이미지는 최대 4개까지만 업로드할 수 있습니다.');
-      }
-    } catch (error) {
-      alert(error);
+    /* 임시로 생성한 함수입니다. 회원가입 기능 구현 시 삭제 */
+    if (uploadedImages.length < 4) {
+      const imageUrl = URL.createObjectURL(uploadFile);
+      setUploadedImages((prevImages) => [...prevImages, imageUrl]);
     }
+
+    /* 회원가입 기능 구현 후 사용할 함수입니다. */
+    //   try {
+    //     const response: ApiResponse = await apiInstance.post('activities/image', formData, {
+    //     });
+    //     // 업로드된 이미지 배열이 4개 이하인 경우에만 추가
+    //     if (uploadedImages.length < 4) {
+    //       setUploadedImages((prevImages) => [...prevImages, response.activityImageUrl]);
+    //     } else {
+    //       alert('이미지는 최대 4개까지만 업로드할 수 있습니다.');
+    //     }
+    //   } catch (error) {
+    //     alert(error);
+    //   }
   };
+
   /** 이미지 삭제 처리 */
   const handleDeleteImage = (index: number) => {
     setUploadedImages((prevImages) => prevImages.filter((_, idx) => idx !== index));
