d151df723b3db5f1ef13d265b1753158cc326111
hakyoung12 <gkr097@naver.com>
2024-07-06 11:35:43 +0900

    refactor: 페이지번호 렌더링로직 수정

--- src/models/pagination/renderPageNumbers.tsx
+++ src/models/pagination/renderPageNumbers.tsx
@@ -8,33 +8,39 @@ interface PageNumbersProps {
 
 /**현재 페이지를 기준으로 좌우 2개씩 총 5개의 페이지 번호 계산 */
 const PageNumbers: React.FC<PageNumbersProps> = ({ currentPage, totalPage, onClick }) => {
-  const pages = [];
-  let startPage = Math.max(1, currentPage - 2);
-  let endPage = Math.min(totalPage, startPage + 4);
+  let start: number;
+  let end: number;
 
-  // 현재 페이지가 첫 페이지인 경우
-  if (currentPage === 1) {
-    endPage = Math.min(totalPage, startPage + 4);
+  // 전페 페이지가 5페이지 이하일 때
+  if (totalPage <= 5) {
+    start = 1;
+    end = totalPage;
+  } else {
+    //시작페이지 렌더링
+    if (currentPage <= 3) {
+      start = 1;
+      end = 5;
+    } //마지막페이지 렌더링
+    else if (currentPage >= totalPage - 2) {
+      start = totalPage - 4;
+      end = totalPage;
+    } //중간페이지 렌더링
+    else {
+      start = currentPage - 2;
+      end = currentPage + 2;
+    }
   }
 
-  // 현재 페이지가 마지막 페이지인 경우
-  if (currentPage === totalPage) {
-    startPage = Math.max(1, currentPage - 4);
-  }
-  // 페이지 숫자 렌더링
-  for (let i = startPage; i <= endPage; i++) {
-    pages.push(
-      <button
-        key={i}
-        className={`py-17 text-base size-[55px] items-center justify-center rounded-2xl border-[1px] border-var-green-dark text-[18pxr] text-var-green-dark max-md:size-[40px] ${i === currentPage ? 'bg-var-green-dark text-white' : 'text-gray-600'}`}
-        onClick={() => onClick(i)}
-      >
-        {i}
-      </button>,
-    );
-  }
-
-  return <>{pages}</>;
+  const numbers = Array.from({ length: end - start + 1 }, (_, i) => start + i);
+  return numbers.map((number) => (
+    <button
+      key={number}
+      className={`py-17 text-base size-[55px] items-center justify-center rounded-2xl border-[1px] border-var-green-dark text-[18pxr] text-var-green-dark max-md:size-[40px] ${number === currentPage ? 'bg-var-green-dark text-white' : 'text-gray-600'}`}
+      onClick={() => onClick(number)}
+    >
+      {number}
+    </button>
+  ));
 };
 
 export default PageNumbers;
