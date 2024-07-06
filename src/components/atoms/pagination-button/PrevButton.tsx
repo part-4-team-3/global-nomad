98d83181a6b7854ce4898636c37caeeeaf703010
hakyoung12 <gkr097@naver.com>
2024-07-06 10:24:20 +0900

    rename: 폴더 이름 수정

---
+++ src/components/atoms/pagination-button/PrevButton.tsx
@@ -0,0 +1,24 @@
+'ues client';
+
+import Image from 'next/image';
+
+interface Props {
+  currentPage?: number;
+  onPrev?: () => void;
+}
+
+export default function PrevButton({ currentPage, onPrev }: Props) {
+  return (
+    <button
+      onClick={onPrev}
+      className="relative size-[55px] md:size-[40px]"
+      disabled={currentPage === 1}
+    >
+      <Image
+        fill
+        src={currentPage === 1 ? '/paginationLeft_invalid.svg' : '/paginationLeft.svg'}
+        alt="이전 페이지"
+      />
+    </button>
+  );
+}
