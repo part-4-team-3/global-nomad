72d742b25687006273866be4a814e78429de15cc
daeun <qzsz797979@gmail.com>
2024-07-07 16:07:30 +0900

    feat: Header 태그구조 구현

---
+++ src/components/templates/header/Header.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Header() {
+  const navList = [
+    {
+      text: '로그인',
+      link: '/signin',
+    },
+    {
+      text: '회원가입',
+      link: '/signup',
+    },
+  ];
+  return (
+    <header className="flex">
+      <h1>
+        <Link href="/">
+          <Image src="/logo.svg" width={165.5} height={28} alt="GlobalNomad logo" />
+        </Link>
+      </h1>
+      <nav>
+        <ul>
+          {navList.map((nav) => (
+            <Link href={nav.link} key={nav.text}>
+              {nav.text}
+            </Link>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+}
