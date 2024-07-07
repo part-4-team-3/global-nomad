ff22dbe897e797e54ec0106f8fd152b44328e35b
ohdong9795 <ohdong9795@naver.com>
2024-07-08 00:37:13 +0900

    feat: add react-toastify

---
+++ src/constant/toast-option.ts
@@ -0,0 +1,9 @@
+import { ToastOptions } from 'react-toastify';
+
+export const TOAST_DEFAULT: ToastOptions = {
+  position: 'top-center',
+  autoClose: 1500,
+  draggable: true,
+  theme: 'dark',
+  progressStyle: { background: 'green' },
+};
