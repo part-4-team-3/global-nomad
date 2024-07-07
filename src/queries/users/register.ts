8cb62b67e71b4f22eca42e1cb1029ecdb166a2cd
ohdong9795 <ohdong9795@naver.com>
2024-07-08 01:13:58 +0900

    feat: add auth success, fail callback

--- src/queries/users/register.ts
+++ src/queries/users/register.ts
@@ -1,6 +1,6 @@
 import { apiInstance } from '@/lib/axios';
+import { onRegisterError } from '@/models/auth/register-models';
 import { UseMutationOptions } from '@tanstack/react-query';
-import { AxiosError, isAxiosError } from 'axios';
 
 export interface User {
   id: string;
@@ -18,21 +18,11 @@ interface RegisterBody {
 }
 
 const register = async (body: RegisterBody) => {
-  const res = await apiInstance.post<User>('/users', body);
-  return res.data;
+  const res = await apiInstance.post<RegisterBody, User>('/users', body);
+  return res;
 };
 
 export const registerMutationOptions: UseMutationOptions<User, Error, RegisterBody> = {
   mutationFn: register,
-  onSuccess: (data) => {
-    alert('회원가입 성공'); // alert은 이후 toast 라이브러리 or 직접 만들어서 교체
-  },
-  onError: (error: Error) => {
-    if (error instanceof AxiosError) {
-      alert(error.response?.data.message);
-      return;
-    }
-
-    alert(error.message);
-  },
+  onError: onRegisterError,
 };
