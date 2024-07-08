1250c8bef147639d8504f0a0f8bf0d1527b9acb7
ohdong9795 <ohdong9795@naver.com>
2024-07-08 15:02:17 +0900

    refactor: Update auth onSuccess, onError callbacks

--- src/models/auth/login-models.ts
+++ src/models/auth/login-models.ts
@@ -1,7 +1,6 @@
 import { TOAST_DEFAULT } from '@/constant/toast-option';
 import { cookie } from '@/lib/cookie';
 import { LoginResponse } from '@/queries/auth/login';
-import { AxiosError } from 'axios';
 import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
 import { toast } from 'react-toastify';
 
@@ -11,15 +10,6 @@ export const onLoginSuccess = (data: LoginResponse, router: AppRouterInstance) =
   router.push('/');
 };
 
-export const onLoginError = (error: Error) => {
-  if (error instanceof AxiosError) {
-    toast(error.response?.data.message, TOAST_DEFAULT);
-    return;
-  }
-
-  const status = Number(error.message);
-
-  if (status === 404) {
-    toast('이메일, 비밀번호가 일치하지 않습니다.', TOAST_DEFAULT);
-  }
+export const onLoginError = () => {
+  toast('이메일, 비밀번호가 일치하지 않습니다.', TOAST_DEFAULT);
 };
