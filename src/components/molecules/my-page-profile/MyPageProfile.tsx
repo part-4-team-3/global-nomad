f6bca212f405297dc0606e276f04709f4deddf84
hakyoung12 <gkr097@naver.com>
2024-07-08 15:56:05 +0900

    feat: 마이페이지 사이드바 클릭이벤트 추가

--- src/components/molecules/my-page-profile/MyPageProfile.tsx
+++ src/components/molecules/my-page-profile/MyPageProfile.tsx
@@ -5,7 +5,7 @@ export default function MyPageProfile() {
     <div className="relative flex items-center justify-center">
       <div className="relative size-[160px] rounded-full">
         {/* 임시이미지 입니다. */}
-        <Image fill src="/CloseButtonIcon.svg" alt="프로필 사진" />
+        <Image fill src="/close-button-icon.svg" alt="프로필 사진" />
         <button className="absolute bottom-[0px] right-[12px] size-[44px]">
           <Image fill src="/profile-image-setting-icon.svg" alt="프로필 사진 수정하기" />
         </button>
