001b90c7b0ec1c79e4d7c4b50175c524503a6d8b
hakyoung12 <gkr097@naver.com>
2024-07-08 12:02:53 +0900

    feat: 마이페이지 사이드바 프로필부분 구현

---
+++ src/components/molecules/my-page-profile/MyPageProfile.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+
+export default function MyPageProfile() {
+  return (
+    <div className="relative flex items-center justify-center">
+      <div className="relative size-[160px] rounded-full">
+        {/* 임시이미지 입니다. */}
+        <Image fill src="/CloseButtonIcon.svg" alt="프로필 사진" />
+        <button className="absolute bottom-[0px] right-[12px] size-[44px]">
+          <Image fill src="/profile-image-setting-icon.svg" alt="프로필 사진 수정하기" />
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/components/organisms/side-navigation-menu/SideNavigationMenu.tsx
+++ src/components/organisms/side-navigation-menu/SideNavigationMenu.tsx
@@ -1,35 +1,46 @@
+import MyPageProfile from '@/components/molecules/my-page-profile/MyPageProfile';
 import Image from 'next/image';
 import Link from 'next/link';
 
 export default function SideNavigationMenu() {
-  const menuStyle = 'flex gap-[14px] py-[9px] px-[16px] text-[16px] font-bold text-var-gray3';
+  const menuStyle =
+    'flex w-full gap-[14px] py-[9px] px-[16px] text-[16pxr] font-bold text-var-gray3';
   return (
-    <div className="p-4 w-1/4 rounded-xl shadow-md">
+    <div className="flex w-1/4 flex-col gap-[24px] rounded-xl p-[24px] shadow-md">
+      <MyPageProfile />
       <nav className="mt-4">
         <ul>
-          <li className={menuStyle}>
-            <div className="relative size-[24px]">
-              <Image fill src="/my-info-icon.svg" alt="내 정보" />
-            </div>
-            <Link href="/mypage">내 정보</Link>
+          <li>
+            <Link href="/mypage" className={menuStyle}>
+              <div className="relative size-[24px]">
+                <Image fill src="/my-info-icon.svg" alt="내 정보" />
+              </div>
+              내 정보
+            </Link>
           </li>
-          <li className={menuStyle}>
-            <div className="relative size-[24px]">
-              <Image fill src="/reservation-icon.svg" alt="예약 내역" />
-            </div>
-            <Link href="/reservation">예약 내역</Link>
+          <li>
+            <Link href="/reservation" className={menuStyle}>
+              <div className="relative size-[24px]">
+                <Image fill src="/reservation-icon.svg" alt="예약 내역" />
+              </div>
+              예약 내역
+            </Link>
           </li>
-          <li className={menuStyle}>
-            <div className="relative size-[24px]">
-              <Image fill src="/my-activity-setting-icon.svg" alt="내 체험 관리" />
-            </div>
-            <Link href="/myactivity">내 체험 관리</Link>
+          <li>
+            <Link href="/myactivity" className={menuStyle}>
+              <div className="relative size-[24px]">
+                <Image fill src="/my-activity-setting-icon.svg" alt="내 체험 관리" />
+              </div>
+              내 체험 관리
+            </Link>
           </li>
-          <li className={menuStyle}>
-            <div className="relative size-[24px]">
-              <Image fill src="/reservation-status-icon.svg" alt="예약 현황" />
-            </div>
-            <Link href="/calendar">예약 현황</Link>
+          <li>
+            <Link href="/calendar" className={menuStyle}>
+              <div className="relative size-[24px]">
+                <Image fill src="/reservation-status-icon.svg" alt="예약 현황" />
+              </div>
+              예약 현황
+            </Link>
           </li>
         </ul>
       </nav>
