ad941150591172b823f827418457aceb601f70f3
daeun <qzsz797979@gmail.com>
2024-07-08 09:53:47 +0900

    feat: ProfileImage 컴포넌트 이미지 없을 때 UI 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import Header from '@/components/templates/header/Header';
 import Image from 'next/image';
 
+const USER = {
+  nickname: '오다은',
+  profileImageUrl: null,
+};
+
 export default function Home() {
   return (
     <>
-      <Header />
+      <Header user={USER} />
     </>
   );
 }
--- src/components/atoms/profile/ProfileImage.tsx
+++ src/components/atoms/profile/ProfileImage.tsx
@@ -1,14 +1,21 @@
 import Image from 'next/image';
 
 interface Props {
-  src: string;
+  nickname: string;
+  imageUrl: string | null;
 }
 
-export default function ProfileImage({ src }: Props) {
+export default function ProfileImage({ nickname, imageUrl }: Props) {
+  if (!imageUrl)
+    return (
+      <div className="flex size-32pxr items-center justify-center rounded-[50%] bg-var-gray1 text-white">
+        {nickname.slice(0, 1)}
+      </div>
+    );
   return (
     <Image
       className="size-32pxr overflow-hidden rounded-[50%] object-cover"
-      src={src}
+      src={imageUrl}
       width={32}
       height={32}
       alt="프로필 이미지"
