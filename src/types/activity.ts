058699245ca88ddc6aef5a198cd2b480854ac8e9
daeun <qzsz797979@gmail.com>
2024-07-08 18:53:26 +0900

    feat: activity type 생성

---
+++ src/types/activity.ts
@@ -0,0 +1,14 @@
+interface Activity {
+  id: number;
+  userId: number;
+  title: string;
+  description: string;
+  category: string;
+  price: number;
+  address: string;
+  bannerImageUrl: string;
+  rating: number;
+  reviewCount: number;
+  createdAt: string;
+  updatedAt: string;
+}
