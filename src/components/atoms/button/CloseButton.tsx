c7c15fd3b7679c89a9a21f6bcb4565eca2e74cc7
hakyoung12 <gkr097@naver.com>
2024-07-10 17:08:48 +0900

    style: 이미지 업로더 스타일 수정

--- src/components/atoms/button/CloseButton.tsx
+++ src/components/atoms/button/CloseButton.tsx
@@ -9,7 +9,7 @@ export default function CloseButton({ id, onClose }: Props) {
   return (
     <button
       onClick={() => onClose(id)}
-      className="max-xl:right-[-16px] max-xl:top-[-16px] max-xl:size-[32px] max-sm:right-[-12px] max-sm:top-[-12px] max-sm:size-[24px] absolute z-10 xl:right-[-20px] xl:top-[-20px] xl:size-[40px]"
+      className="absolute right-[-12px] top-[-12px] z-10 size-[24px] md:right-[-16px] md:top-[-16px] md:size-[32px] lg:right-[-20px] lg:top-[-20px] lg:size-[40px]"
     >
       <Image fill src="/close-button-icon.svg" alt="등록 이미지 삭제" />
     </button>
--- src/components/molecules/generated-image/GeneratedImage.tsx
+++ src/components/molecules/generated-image/GeneratedImage.tsx
@@ -9,13 +9,11 @@ interface Props {
 export default function GeneratedImage({ key, url, onClose }: Props) {
   return (
     <>
-      <div className="relative">
+      <div className="relative size-[167px] rounded-[24px] md:size-[206px] lg:size-[180px]">
         <CloseButton id={key} onClose={() => onClose(key)} />
-        {url && (
-          <div className="max-xl:size-[206px] max-sm:size-[167px] relative overflow-hidden rounded-[24px] xl:size-[180px]">
-            <Image fill src={url} alt="이미지 생성" className="object-cover" />
-          </div>
-        )}
+        <div className="relative size-[167px] overflow-hidden rounded-[24px] md:size-[206px] lg:size-[180px]">
+          {url && <Image fill src={url} alt="이미지 생성" className="object-cover" />}
+        </div>
       </div>
     </>
   );
