3d48ade501f1c3f5b09f9f69d9ea8383f1945bdc
bk-git-hub <bkprivate99@gmail.com>
2024-07-11 11:28:11 +0900

    feat: 날짜 선택시 버튼 텍스트 변경기능 추가

--- src/components/molecules/activity-reservation-selector/ActivityReservationSelector.tsx
+++ src/components/molecules/activity-reservation-selector/ActivityReservationSelector.tsx
@@ -4,25 +4,24 @@ import { format } from 'date-fns';
 import { Calendar } from '@/components/ui/calendar';
 import { Schedule } from '@/types/schedule';
 import Image from 'next/image';
-import {
-  createScheduleHashMap,
-  ScheduleHashMap,
-} from '@/models/activity-reservation/create-schedule-hash-map';
+import { ScheduleHashMap } from '@/models/activity-reservation/create-schedule-hash-map';
 import ScheduleButton from '@/components/atoms/schedule-button/ScheduleButton';
 import { useState } from 'react';
+import Button from '@/components/atoms/button/Button';
 
 interface Props {
   schedules?: Schedule[];
   scheduledDates: Date[];
   scheduleHash: ScheduleHashMap;
   onClose: () => void;
+  onSelect: (schedule: Schedule | undefined) => void;
 }
 
 export function ActivityReservationSelector({
-  schedules = [],
   scheduledDates,
   scheduleHash,
   onClose,
+  onSelect,
 }: Props) {
   const [date, setDate] = useState<Date | undefined>(undefined);
   const [selectedSchedule, setSelectedSchedule] = useState<Schedule | undefined>();
@@ -31,8 +30,13 @@ export function ActivityReservationSelector({
 
   const message = date ? '해당 날짜에 가능한 스케줄이 없습니다' : '날짜를 선택해주세요';
 
+  const handleAccept = () => {
+    onSelect(selectedSchedule);
+    onClose();
+  };
+
   return (
-    <div className="fixed left-[0px] right-[0px] top-[0px] z-10 h-screen w-screen bg-white px-24pxr py-32pxr">
+    <div className="fixed left-[0px] right-[0px] top-[0px] z-10 flex h-screen w-screen flex-col justify-between bg-white px-24pxr py-32pxr">
       <div className="flex w-full flex-col gap-24pxr">
         <div className="flex w-full items-center justify-between">
           {' '}
@@ -67,6 +71,13 @@ export function ActivityReservationSelector({
           <p className="w-full text-center">{message}</p>
         )}
       </div>
+      <Button
+        text="확인"
+        color="black"
+        onClick={handleAccept}
+        disabled={!selectedSchedule}
+        className="py-15pxr"
+      />
     </div>
   );
 }
--- src/components/organisms/activity-reservation-bar/ActivityReservationBar.tsx
+++ src/components/organisms/activity-reservation-bar/ActivityReservationBar.tsx
@@ -4,6 +4,8 @@ import { useState } from 'react';
 import Button from '@/components/atoms/button/Button';
 import { ScheduleHashMap } from '@/models/activity-reservation/create-schedule-hash-map';
 import { ActivityReservationSelector } from '@/components/molecules/activity-reservation-selector/ActivityReservationSelector';
+import { Schedule } from '@/types/schedule';
+import { format } from 'date-fns';
 
 interface Props {
   price: number;
@@ -14,6 +16,11 @@ interface Props {
 export default function ActivityReservationBar({ price, scheduleHash, scheduledDates }: Props) {
   const [isScheduleSelectorOpen, setIsScheduleSelectorOpon] = useState<boolean>(false);
   const [isNumberSelectorOpen, setIsNumberSelectorOpon] = useState<boolean>(false);
+  const [selectedSchedule, setSelectedSchedule] = useState<Schedule | undefined>();
+
+  const dateButtonText = selectedSchedule
+    ? `${format(selectedSchedule.date, 'yy/MM/dd')} ${selectedSchedule.startTime} ~ ${selectedSchedule.endTime}`
+    : '날짜 선택하기';
 
   return (
     <div className="fixed bottom-[0px] left-[0px] right-[0px] flex w-full justify-between border-t border-[#a1a1a1] p-[16px]">
@@ -27,7 +34,7 @@ export default function ActivityReservationBar({ price, scheduleHash, scheduledD
           className="w-fit text-14pxr font-[600] text-var-green-dark underline"
           onClick={() => setIsScheduleSelectorOpon(true)}
         >
-          날짜 선택하기
+          {dateButtonText}
         </button>
       </div>
 
@@ -35,7 +42,7 @@ export default function ActivityReservationBar({ price, scheduleHash, scheduledD
         text="예약하기"
         color="black"
         className="px-24pxr py-14pxr disabled:bg-var-gray3"
-        disabled={true}
+        disabled={!selectedSchedule}
       ></Button>
 
       {isScheduleSelectorOpen && (
@@ -43,6 +50,7 @@ export default function ActivityReservationBar({ price, scheduleHash, scheduledD
           scheduleHash={scheduleHash}
           scheduledDates={scheduledDates}
           onClose={() => setIsScheduleSelectorOpon(false)}
+          onSelect={(schedule: Schedule | undefined) => setSelectedSchedule(schedule)}
         />
       )}
     </div>
