164252c18f645e566a23a69c304aefd3c914bb31
daeun <qzsz797979@gmail.com>
2024-07-11 10:48:02 +0900

    design: ActivityCard 컴포넌트 인기체험 버전 반응형 완료

--- src/components/molecules/card/ActivityCard.tsx
+++ src/components/molecules/card/ActivityCard.tsx
@@ -4,7 +4,10 @@ import Link from 'next/link';
 
 export default function ActivityCard() {
   return (
-    <Link href="" className="relative flex size-384pxr overflow-hidden rounded-[20px] bg-var-black">
+    <Link
+      href=""
+      className="relative flex size-186pxr overflow-hidden rounded-[20px] bg-var-black sm:size-384pxr"
+    >
       <Image
         src="https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/globalnomad/activity_registration_image/5-3_547_1720439740814.png"
         width={384}
@@ -13,18 +16,18 @@ export default function ActivityCard() {
         className="absolute size-full object-cover"
       />
       <div
-        className="relative flex flex-col justify-end gap-20pxr px-20pxr py-30pxr"
+        className="relative flex flex-col justify-end gap-6pxr px-20pxr py-24pxr sm:gap-20pxr sm:py-30pxr"
         style={{
           background:
             'linear-gradient(180deg, rgba(0, 0, 0, 0.00) 33.33%, rgba(0, 0, 0, 0.80) 91.67%)',
         }}
       >
         <ReviewRating color="white" />
-        <h3 className="w-[80%] break-keep text-30pxr font-[700] leading-[120%] text-white">
+        <h3 className="w-full break-keep text-18pxr font-[700] leading-[120%] text-white sm:w-[80%] sm:text-30pxr">
           함께 배우면 즐거운 스트릿 댄스
         </h3>
         <div className="flex items-center gap-5pxr">
-          <b className="text-20pxr font-[700] text-white">₩ 190000</b>
+          <b className="text-16pxr font-[700] text-white sm:text-20pxr">₩ 190000</b>
           <span className="text-var-gray9 text-14pxr">/ 인</span>
         </div>
       </div>
