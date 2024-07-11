e6ba1d60aaecafba0be28a9a6275e43fc56987d6
bk-git-hub <bkprivate99@gmail.com>
2024-07-11 10:25:32 +0900

    feat: 데이터를 프롭으로 넘겨받게 설정

--- src/components/molecules/activity-reservation-selector/ActivityReservationSelector.tsx
+++ src/components/molecules/activity-reservation-selector/ActivityReservationSelector.tsx
@@ -1,38 +1,60 @@
 'use client';
 
-import * as React from 'react';
 import { format } from 'date-fns';
 import { Calendar } from '@/components/ui/calendar';
-
-type schedule = {
-  id: number;
-  date: string;
-  startTime: '12:00';
-  endTime: '13:00';
-};
+import { Schedule } from '@/types/schedule';
+import { parseISO } from 'date-fns';
+import {
+  createScheduleHashMap,
+  ScheduleHashMap,
+} from '@/models/activity-reservation/create-schedule-hash-map';
+import ScheduleButton from '@/components/atoms/schedule-button/ScheduleButton';
+import { useState } from 'react';
 
 interface Props {
-  scheduled?: Date[];
+  schedules?: Schedule[];
+  scheduledDates: Date[];
+  scheduleHash: ScheduleHashMap;
 }
 
-export function ActivityCalendar({ scheduled }: Props) {
-  const [date, setDate] = React.useState<Date | undefined>(new Date());
+export function ActivityReservationSelector({
+  schedules = [],
+  scheduledDates,
+  scheduleHash,
+}: Props) {
+  const [date, setDate] = useState<Date | undefined>(undefined);
+  const [selectedSchedule, setSelectedSchedule] = useState<Schedule | undefined>();
 
   const formattedDate = date ? format(date, 'yyyy-MM-dd') : '';
 
+  const message = date ? '해당 날짜에 가능한 스케줄이 없습니다' : '날짜를 선택해주세요';
+
   return (
-    <div>
-      <h2>날짜</h2>
+    <div className="flex w-fit flex-col gap-24pxr px-24pxr py-32pxr">
+      <h2 className="text-28pxr font-[700]">날짜</h2>
       <Calendar
         mode="single"
-        scheduled={scheduled}
+        scheduled={scheduledDates}
         selected={date}
         onSelect={setDate}
         className="w-fit rounded-md border shadow"
       />
-      {date && <p>{formattedDate}</p>}
 
       <p className="text-18pxr font-[700]">예약 가능한 시간</p>
+      {formattedDate != '' && scheduleHash[formattedDate] ? (
+        <div className="flex gap-12pxr">
+          {scheduleHash[formattedDate].map((schedule) => (
+            <ScheduleButton
+              key={schedule.id}
+              onClick={() => setSelectedSchedule({ ...schedule, date: formattedDate })}
+            >
+              {schedule.startTime}~{schedule.endTime}
+            </ScheduleButton>
+          ))}
+        </div>
+      ) : (
+        <p className="w-full text-center">{message}</p>
+      )}
     </div>
   );
 }
