8b1c3068715d6de3d0aeca635ed7566c7a33e1b9
striker1826 <alstjq123579@daum.net>
2024-07-14 15:57:27 +0900

    feat: redis를 이용한 refreshToken 재요청 로직들 추가 - axios, middleware

---
+++ src/middleware.ts
@@ -0,0 +1,15 @@
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(req: NextRequest) {
+  const userId = cookies().get('userId');
+  if (!userId?.value) {
+    return NextResponse.redirect(new URL('/signin', req.url));
+  }
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ['/activity/:path*', '/reservation'],
+};
