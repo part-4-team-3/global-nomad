c5298930a388d5b51231714fdca93a7a29d35cfb
bk-git-hub <bkprivate99@gmail.com>
2024-07-15 19:52:29 +0900

    chore: 임시로 컴포넌트 볼수있는 페이지 설정

--- src/app/(main)/activity/[id]/page.tsx
+++ src/app/(main)/activity/[id]/page.tsx
@@ -4,11 +4,9 @@ import ActivityReservationBar from '@/components/organisms/activity-reservation-
 import { parseISO } from 'date-fns';
 import { createScheduleHashMap } from '@/models/activity-reservation/create-schedule-hash-map';
 import { getActivityDetails } from '@/queries/activities/get-activity-details';
-import ParticipantCounter from '@/components/molecules/participant-counter/ParticipantCounter';
-import ReviewCard from '@/components/molecules/review-card/ReviewCard';
-import ReviewList from '@/components/organisms/review-list/ReviewList';
-import ReviewPagination from '@/components/templates/review-template/ReviewTemplate';
 import ReviewTemplate from '@/components/templates/review-template/ReviewTemplate';
+import Map from '@/components/molecules/map/Map';
+import ActivityDescription from '@/components/molecules/activity-description/ActivityDescription';
 interface Props {
   params: { id: number };
   searchParams: { [key: string]: string | string[] | undefined };
@@ -18,7 +16,8 @@ const mockData = {
   id: 7,
   userId: 21,
   title: '함께 배우면 즐거운 스트릿댄스',
-  description: '둠칫 둠칫 두둠칫',
+  description:
+    '안녕하세요! 저희 스트릿 댄스 체험을 소개합니다. 저희는 신나고 재미있는 스트릿 댄스 스타일을 가르칩니다. 크럼프는 세계적으로 인기 있는 댄스 스타일로, 어디서든 춤출 수 있습니다. 저희 체험에서는 새로운 스타일을 접할 수 있고, 즐거운 시간을 보낼 수 있습니다. 저희는 초보자부터 전문가까지 어떤 수준의 춤추는 사람도 가르칠 수 있도록 준비해놓았습니다. 저희와 함께 즐길 수 있는 시간을 기대해주세요! 각종 음악에 적합한 스타일로, 저희는 크럼프 외에도 전통적인 스트릿 댄스 스타일과 최신 스트릿 댄스 스타일까지 가르칠 수 있습니다. 저희 체험에서는 전문가가 직접 강사로 참여하기 때문에, 저희가 제공하는 코스는 어떤 수준의 춤추는 사람도 쉽게 이해할 수 있도록 준비해놓았습니다. 저희 체험을 참가하게 된다면, 즐거운 시간 뿐만 아니라 새로운 스타일을 접할 수 있을 것입니다',
   category: '투어',
   price: 10000,
   address: '서울특별시 강남구 테헤란로 427',
@@ -164,12 +163,14 @@ export default async function Page({ params, searchParams }: Props) {
         reviewCount={mockData.reviewCount}
       />
       <ImageCarousel bannerImg={mockData.bannerImageUrl} subImg={mockData.subImageUrls} />
-
+      <ActivityDescription description={mockData.description} />
       <ActivityReservationBar
         price={mockData.price}
         scheduleHash={scheduleHash}
         scheduledDates={scheduledDates}
       />
+
+      <Map address={mockData.address} />
 
       <ReviewTemplate
         ratings={4.5}
