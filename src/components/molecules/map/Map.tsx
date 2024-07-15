4d3c146bf486ae458c513c16d768885acab144bd
bk-git-hub <bkprivate99@gmail.com>
2024-07-15 19:40:24 +0900

    feat: 카카오맵 보여주는 컴포넌트 생성

---
+++ src/components/molecules/map/Map.tsx
@@ -0,0 +1,61 @@
+'use client';
+import AddressLabel from '@/components/atoms/address-label/AddressLabel';
+import { useEffect, useRef } from 'react';
+
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
+
+interface Props {
+  address: string;
+}
+
+export default function Map({ address }: Props) {
+  const mapRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    window.kakao.maps.load(() => {
+      const options = {
+        //지도를 생성할 때 필요한 기본 옵션
+        center: new window.kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+        level: 3, //지도의 레벨(확대, 축소 정도)
+      };
+
+      const map = new window.kakao.maps.Map(mapRef.current, options); //지도 생성 및 객체 리턴
+
+      const geocoder = new window.kakao.maps.services.Geocoder();
+
+      geocoder.addressSearch(address, function (result: any, status: any) {
+        // 정상적으로 검색이 완료됐으면
+        if (status === window.kakao.maps.services.Status.OK) {
+          console.log(result);
+          const coords = new window.kakao.maps.LatLng(result[0].y, result[0].x);
+
+          // 결과값으로 받은 위치를 마커로 표시합니다
+          const marker = new window.kakao.maps.Marker({
+            map: map,
+            position: coords,
+          });
+
+          // 인포윈도우로 장소에 대한 설명을 표시합니다
+          const infowindow = new window.kakao.maps.InfoWindow({
+            content: `<div style="width:150px;text-align:center;padding:6px 0;">${result[0].road_address.building_name}</div>`,
+          });
+          infowindow.open(map, marker);
+
+          // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+          map.setCenter(coords);
+        }
+      });
+    });
+  }, [address]);
+
+  return (
+    <div className="flex w-full max-w-450pxr flex-col gap-[8px] px-[24px] py-[16px]">
+      <div className="h-450pxr w-full" ref={mapRef}></div>
+      <AddressLabel address={address} />
+    </div>
+  );
+}
