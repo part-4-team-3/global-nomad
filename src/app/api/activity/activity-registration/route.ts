da3f1795feaaa0fad01011d824522b384c5691bb
hakyoung12 <gkr097@naver.com>
2024-07-16 13:16:36 +0900

    chore: route 파일 삭제

--- src/app/api/activity/activity-registration/route.ts
+++
@@ -1,6 +0,0 @@
-import { handleRequest } from '@/app/(action)/axios';
-import { ActivitySettingData } from '@/types/activity';
-
-export async function POST(body: ActivitySettingData) {
-  return handleRequest('/activities', 'post', body);
-}
