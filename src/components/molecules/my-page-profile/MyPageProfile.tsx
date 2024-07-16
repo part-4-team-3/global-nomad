733d0d210596c476ec76258ddb906809f2fd443c
hakyoung12 <gkr097@naver.com>
2024-07-16 12:16:02 +0900

    chore: 충돌해결

--- src/components/molecules/my-page-profile/MyPageProfile.tsx
+++ src/components/molecules/my-page-profile/MyPageProfile.tsx
@@ -1,22 +1,13 @@
 import EditProfileImage from '@/components/atoms/edit-profile-image/EditProfileImage';
 import Image from 'next/image';
-import { usePathname } from 'next/navigation';
-import useUser from '@/store/useUser';
 
 export default function MyPageProfile() {
-  const { user } = useUser();
-  // const pathname = usePathname();
   return (
     <div className="relative flex items-center justify-center">
-      <div className="relative size-160pxr rounded-full">
-        {user && user.profileImageUrl ? (
-          <Image fill src={user.profileImageUrl} alt="프로필 사진" />
-        ) : (
-          <div className="flex size-160pxr items-center justify-center rounded-[50%] bg-var-gray1 text-80pxr text-white">
-            {user && user.nickname.slice(0, 1)}
-          </div>
-        )}
-        <EditProfileImage />
+      <div className="relative size-[160px] rounded-full">
+        {/* 임시이미지입니다. */}
+        <Image fill src="/close-button-icon.svg" alt="프로필 사진" />
+        \<EditProfileImage />
       </div>
     </div>
   );
