968414da447ad3a7d7b9453fa06a38a35c48f235
ohdong9795 <ohdong9795@naver.com>
2024-07-16 11:32:47 +0900

    feat: add infinite scrolling with useInfiniteQuery and useInView

---
+++ src/components/templates/reservation/Header.tsx
@@ -0,0 +1,32 @@
+import DropdownMenu from '@/components/molecules/dropdown-menu/DropdownMenu';
+import { RESERVATION_LABELS } from '@/constant/reservation-labels';
+import { ReservationStatus, isReservationStatus } from '@/types/reservation';
+
+interface Props {
+  setStatus: (status: ReservationStatus | null) => void;
+}
+
+export default function Header({ setStatus }: Props) {
+  return (
+    <div className="flex h-53pxr w-full items-center justify-between">
+      <h1 className="h-38pxr text-32pxr font-bold">예약 내역</h1>
+      <div className="h-53pxr w-160pxr text-18pxr">
+        <DropdownMenu text="상태">
+          <button className="w-full" onClick={() => setStatus(null)}>
+            전체
+          </button>
+          {Object.entries(RESERVATION_LABELS).map(([key, label]) => {
+            if (isReservationStatus(key)) {
+              return (
+                <button className="w-full" key={key} onClick={() => setStatus(key)}>
+                  {label}
+                </button>
+              );
+            }
+            return null;
+          })}
+        </DropdownMenu>
+      </div>
+    </div>
+  );
+}
