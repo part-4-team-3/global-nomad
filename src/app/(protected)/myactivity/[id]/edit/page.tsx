317ae7e090879dc00a94e5cb10edeb5cf0ee1028
hakyoung12 <gkr097@naver.com>
2024-07-16 14:11:52 +0900

    remove: 폴더 삭제

--- src/app/(protected)/myactivity/[id]/edit/page.tsx
+++
@@ -1,8 +0,0 @@
-import ActivityRegistrationForm from '@/components/organisms/activity-registration-form/ActivityRegistrationForm';
-
-export default function Page() {
-  {
-    /* 임시입니다.*/
-  }
-  return <ActivityRegistrationForm />;
-}
--- src/app/(protected)/myactivity/post/page.tsx
+++
@@ -1,5 +0,0 @@
-import ActivityRegistrationForm from '@/components/organisms/activity-registration-form/ActivityRegistrationForm';
-
-export default function Page() {
-  return <ActivityRegistrationForm />;
-}
