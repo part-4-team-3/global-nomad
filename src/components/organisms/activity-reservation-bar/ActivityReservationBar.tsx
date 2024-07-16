616a0176abc98278120fa9afbfd677b77198e775
bk-git-hub <bkprivate99@gmail.com>
2024-07-16 18:58:49 +0900

    refactor: 가격표시를 컴포넌트로 분리

--- src/components/organisms/activity-reservation-bar/ActivityReservationBar.tsx
+++ src/components/organisms/activity-reservation-bar/ActivityReservationBar.tsx
@@ -8,6 +8,7 @@ import { Schedule } from '@/types/schedule';
 import { format } from 'date-fns';
 import { ActivityParticipantSelector } from '@/components/molecules/activity-participant-selector/ActivityParticipantSelector';
 import { useReservation } from '@/models/activity-reservation/use-reservation';
+import PriceDisplay from '@/components/atoms/price-display/PriceDisplay';
 
 interface Props {
   price: number;
@@ -18,9 +19,7 @@ interface Props {
 export default function ActivityReservationBar({ price, scheduleHash, scheduledDates }: Props) {
   const [isScheduleSelectorOpen, setIsScheduleSelectorOpen] = useState<boolean>(false);
   const [isParticipantSelectorOpen, setIsParticipantSelectorOpen] = useState<boolean>(false);
-  const {selectedSchedule, setSelectedSchedule, participants, setParticipants} = useReservation();
-
-
+  const { selectedSchedule, setSelectedSchedule, participants, setParticipants } = useReservation();
 
   const dateButtonText = selectedSchedule
     ? `${format(selectedSchedule.date, 'yy/MM/dd')} ${selectedSchedule.startTime} ~ ${selectedSchedule.endTime}`
@@ -29,9 +28,9 @@ export default function ActivityReservationBar({ price, scheduleHash, scheduledD
   return (
     <div className="fixed bottom-[0px] left-[0px] right-[0px] z-10 flex w-screen justify-between border-t border-[#a1a1a1] bg-white p-[16px]">
       <div className="flex flex-col gap-8pxr">
-        <div className="flex gap-6pxr text-20pxr font-[700] leading-[26px]">
-          <span>₩{price * participants}</span>
-          <span>/</span>
+        <div className="flex gap-6pxr font-[500]">
+          <PriceDisplay price={price * participants} />
+          <span className="text-20pxr font-[600]">/</span>
           <button
             className="text-18pxr text-var-green-dark underline"
             onClick={() => setIsParticipantSelectorOpen(true)}
