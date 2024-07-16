f090a64ae51103c894a9eb548c937dbeca02ecc9
daeun <qzsz797979@gmail.com>
2024-07-16 11:27:01 +0900

    design: 스크롤 UI 완료

--- src/components/organisms/card-list/BestActivityCardList.tsx
+++ src/components/organisms/card-list/BestActivityCardList.tsx
@@ -9,13 +9,15 @@ interface Props {
 
 export default function BestActivityCardList({ activitiesData, carouselRef }: Props) {
   return (
-    <div
-      ref={carouselRef}
-      className="flex w-full snap-x snap-mandatory gap-[16px] overflow-x-auto scrollbar-hide md:gap-[24px] md:px-[24px]"
-    >
-      {activitiesData.activities.map((activity) => (
-        <ActivityCard key={activity.id} activity={activity} isBest />
-      ))}
+    <div className="w-full">
+      <div
+        ref={carouselRef}
+        className="flex snap-x snap-mandatory scroll-px-[16px] gap-[16px] overflow-x-auto px-[16px] scrollbar-hide md:scroll-px-[24px] md:gap-[24px] md:px-[24px]"
+      >
+        {activitiesData.activities.map((activity) => (
+          <ActivityCard key={activity.id} activity={activity} isBest />
+        ))}
+      </div>
     </div>
   );
 }
