fbbf3fa4df5c749ea868339392528f1a8797fb87
ohdong9795 <ohdong9795@naver.com>
2024-07-17 09:16:01 +0900

    feat: add tailwind custom shdow

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -103,6 +103,9 @@ const config = {
       inset: {
         unset: 'unset',
       },
+      boxShadow: {
+        custom: '0 10px 20px rgba(0, 0, 0, 0.15)',
+      },
     },
   },
   plugins: [require('tailwindcss-animate')],
