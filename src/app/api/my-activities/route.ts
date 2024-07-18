287c085d307ad95014b5665383fea1bd77f92ab0
striker1826 <alstjq123579@daum.net>
2024-07-18 13:13:03 +0900

    feat: 내 체험 리스트 API Route에 queryString 추가

--- src/app/api/my-activities/route.ts
+++ src/app/api/my-activities/route.ts
@@ -1,5 +1,10 @@
 import { handleRequest } from '@/app/(action)/axios';
+import makeQueryString from '@/lib/query-string';
+import { NextRequest } from 'next/server';
 
-export async function GET() {
-  return handleRequest('my-activities', 'get');
+export async function GET(req: NextRequest) {
+  const queryParams = Object.fromEntries(req.nextUrl.searchParams);
+  const queryString = makeQueryString(queryParams);
+
+  return handleRequest(`my-activities${queryString}`, 'get');
 }
