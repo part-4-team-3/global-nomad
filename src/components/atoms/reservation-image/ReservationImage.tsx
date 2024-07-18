6ebf451c1304e17371b351358e6f51da2d4367d8
ohdong9795 <ohdong9795@naver.com>
2024-07-18 11:46:32 +0900

    feat: add review modal

--- src/components/atoms/reservation-image/ReservationImage.tsx
+++ src/components/atoms/reservation-image/ReservationImage.tsx
@@ -2,14 +2,20 @@ import Image from 'next/image';
 import { useState } from 'react';
 
 interface Props {
+  variant: 'card' | 'modal';
   src: string;
 }
 
-export default function ReservationImage({ src }: Props) {
+const SIZE = {
+  card: 'size-128pxr md:size-156pxr lg:size-204pxr',
+  modal: 'size-100pxr md:size-126pxr',
+};
+
+export default function ReservationImage({ variant, src }: Props) {
   const [isError, setIsError] = useState(false);
 
   return (
-    <div className="relative size-128pxr md:size-156pxr lg:size-204pxr">
+    <div className={`relative ${SIZE[variant]}`}>
       <Image
         src={isError ? '/no-image.jpg' : src}
         layout="fill"
