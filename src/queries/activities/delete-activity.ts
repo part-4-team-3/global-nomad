fc75475490e652bc481dd445d39e13a104c55807
bk-git-hub <bkprivate99@gmail.com>
2024-07-18 19:17:13 +0900

    fix: 체험 삭제 api 반환값 변경

--- src/queries/activities/delete-activity.ts
+++ src/queries/activities/delete-activity.ts
@@ -7,10 +7,10 @@ const deleteActivity = async (id: number) => {
 
   try {
     res = await instance.delete(`activities/${id}`);
+
     return '삭제가 완료되었습니다';
   } catch (err) {
     if (err instanceof AxiosError) {
-      console.log(err.response?.data.data.message);
       return err.response?.data.data.message;
     }
   }
