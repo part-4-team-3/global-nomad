4c86635fdcaeffa1cdb48b4087c3f87d32010c17
bk-git-hub <bkprivate99@gmail.com>
2024-07-18 19:14:37 +0900

    feat: 삭제/수정링크 기능추가

--- src/components/organisms/activity-header/ActivityHeader.tsx
+++ src/components/organisms/activity-header/ActivityHeader.tsx
@@ -1,8 +1,13 @@
 'use client';
 
 import AddressLabel from '@/components/atoms/address-label/AddressLabel';
+import Link from 'next/link';
 import Image from 'next/image';
 import { useState } from 'react';
+import { deleteActivity } from '@/queries/activities/delete-activity';
+import { toast } from 'react-toastify';
+import { useRouter } from 'next/navigation';
+import { revalidate } from '@/lib/revalidate';
 
 interface Props {
   category: string;
@@ -10,12 +15,34 @@ interface Props {
   rating: number;
   address: string;
   reviewCount: number;
+  activityId: number;
 }
 
-export default function ActivityHeader({ category, title, rating, address, reviewCount }: Props) {
+export default function ActivityHeader({
+  category,
+  title,
+  rating,
+  address,
+  reviewCount,
+  activityId,
+}: Props) {
   const [optionOpen, setOptionOpen] = useState(false);
   const handleOptionClick = () => {
     setOptionOpen((prev) => !prev);
+  };
+
+  const router = useRouter();
+
+  const handleDelete = async () => {
+    const message: any = await deleteActivity(activityId);
+
+    if (message === '삭제가 완료되었습니다') {
+      await revalidate('/');
+      toast(message);
+      router.push('/');
+    } else {
+      toast(message);
+    }
   };
   return (
     <div className="flex w-full justify-between p-[16px] text-[#112211] md:p-[24px]">
@@ -38,11 +65,15 @@ export default function ActivityHeader({ category, title, rating, address, revie
         </button>
         <div className="h-1pxr w-1pxr">
           {optionOpen && (
-            <div className="border-var-gray-6 font-500 relative right-[180px] top-[20px] w-160pxr rounded-[6px] border bg-white text-18pxr">
-              <button className="border-var-gray-6 w-full shrink-0 border-b px-46pxr py-18pxr">
-                수정하기
+            <div className="border-var-gray-6 font-500 relative right-[180px] top-[20px] z-20 w-160pxr rounded-[6px] border bg-white text-18pxr">
+              <Link href={`/myactivity/${activityId}/edit`}>
+                <button className="border-var-gray-6 w-full shrink-0 border-b px-46pxr py-18pxr">
+                  수정하기
+                </button>
+              </Link>
+              <button onClick={handleDelete} className="w-full px-46pxr py-18pxr">
+                삭제하기
               </button>
-              <button className="w-full px-46pxr py-18pxr">삭제하기</button>
             </div>
           )}
         </div>
