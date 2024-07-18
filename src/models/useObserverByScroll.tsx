7d27aeef2009936942cf05b3ff5a98e10945aa0b
striker1826 <alstjq123579@daum.net>
2024-07-18 13:57:40 +0900

    feat: 내 체험 리스트 무한 스크롤 구현 & 무한 스크롤 훅 구현

---
+++ src/models/useObserverByScroll.tsx
@@ -0,0 +1,38 @@
+import { useEffect } from 'react';
+
+interface ObserverByScrollTypes {
+  isLoading: boolean;
+  ref: any;
+  fetchNextPage: any;
+}
+
+export const useObserverByScroll = ({ isLoading, ref, fetchNextPage }: ObserverByScrollTypes) => {
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 1.0,
+    };
+
+    const handleObserver: IntersectionObserverCallback = (entries) => {
+      const target = entries[0];
+
+      if (target.isIntersecting && !isLoading) {
+        fetchNextPage();
+      }
+    };
+
+    const observer = new IntersectionObserver(handleObserver, options);
+    const currentDivRef = ref.current;
+
+    if (currentDivRef) {
+      observer.observe(currentDivRef);
+    }
+
+    return () => {
+      if (currentDivRef) {
+        observer.unobserve(currentDivRef);
+      }
+    };
+  }, [isLoading, ref, fetchNextPage]);
+};
