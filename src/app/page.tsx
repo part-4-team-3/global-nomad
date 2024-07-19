6fa0fb968e4fd1b5d3dfa9dc7f27e5dfaa8bd3e7
daeun <qzsz797979@gmail.com>
2024-07-19 12:36:12 +0900

    style: InnerLayout을 SearchActivity 내부에 삽입

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,9 +23,7 @@ export default async function Home({ searchParams }: Props) {
     <>
       <Header />
       <Banner activity={bestActivitiesData.activities[0]} />
-      <InnerLayout>
-        <SearchActivity />
-      </InnerLayout>
+      <SearchActivity />
       <BestActivities activitiesData={bestActivitiesData} />
       <FilteredActivities searchParams={searchParams} />
       <Footer />
--- src/components/molecules/search/SearchActivity.tsx
+++ src/components/molecules/search/SearchActivity.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Button from '@/components/atoms/button/Button';
+import InnerLayout from '@/components/atoms/inner-layout/InnerLayout';
 import SearchInput from '@/components/atoms/input/SearchInput';
 
 export default function SearchActivity() {
@@ -8,17 +9,19 @@ export default function SearchActivity() {
   const handleChange = () => {};
 
   return (
-    <div className="relative -mt-[60px] flex flex-col gap-[15px] rounded-[16px] bg-white px-[24px] py-[16px] shadow-[0_4px_16px_0_rgba(17,34,17,0.05)] md:gap-[30px] md:py-[32px]">
-      <h3 className="text-16pxr font-[700] md:text-20pxr">무엇을 체험하고 싶으신가요?</h3>
-      <form onSubmit={handleSubmit} className="flex w-full gap-[12px]">
-        <SearchInput onChange={handleChange} />
-        <Button
-          text="검색하기"
-          color="black"
-          type="submit"
-          className="h-56pxr shrink-0 !rounded-[4px] px-[20px] md:px-[40px]"
-        />
-      </form>
-    </div>
+    <InnerLayout>
+      <div className="relative -mt-[60px] flex flex-col gap-[15px] rounded-[16px] bg-white px-[24px] py-[16px] shadow-[0_4px_16px_0_rgba(17,34,17,0.05)] md:gap-[30px] md:py-[32px]">
+        <h3 className="text-16pxr font-[700] md:text-20pxr">무엇을 체험하고 싶으신가요?</h3>
+        <form onSubmit={handleSubmit} className="flex w-full gap-[12px]">
+          <SearchInput onChange={handleChange} />
+          <Button
+            text="검색하기"
+            color="black"
+            type="submit"
+            className="h-56pxr shrink-0 !rounded-[4px] px-[20px] md:px-[40px]"
+          />
+        </form>
+      </div>
+    </InnerLayout>
   );
 }
