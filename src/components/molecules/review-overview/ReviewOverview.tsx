818cb41222b3836342150f3afd1bf3fd34314a8a
bk-git-hub <bkprivate99@gmail.com>
2024-07-20 14:29:39 +0900

    refactor: Add data tags to reviewCount and rating

--- src/components/molecules/review-overview/ReviewOverview.tsx
+++ src/components/molecules/review-overview/ReviewOverview.tsx
@@ -13,12 +13,14 @@ export default function ReviewOverview({ ratings, reviewCount }: Props) {
     <div className="flex w-full flex-col gap-[24px] px-[24px]">
       <span className="text-20pxr font-[700]">후기</span>
       <div className="flex items-center gap-[16px]">
-        <span className="text-50pxr font-[600]">{ratings}</span>
+        <data className="text-50pxr font-[600]" value={ratings}>
+          {ratings}
+        </data>
         <div className="flex flex-col gap-[8px]">
           <span>{rateText}</span>
           <div className="flex gap-[6px]">
             <Image src="/star-icon.svg" width={16} height={16} alt="" />
-            <span>{formattedReviewCount}개 후기</span>
+            <data value={reviewCount}>{formattedReviewCount}개 후기</data>
           </div>
         </div>
       </div>
