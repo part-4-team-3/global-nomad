57b2c24a55f67fc3f84a13e60aea2d6600175eef
daeun <qzsz797979@gmail.com>
2024-07-21 15:49:30 +0900

    feat: 검색했을 때 카드리스트 제목 구현

--- src/components/templates/main/SearchedActivities.tsx
+++ src/components/templates/main/SearchedActivities.tsx
@@ -10,13 +10,19 @@ interface Props {
 export default async function SearchedActivities({ searchParams }: Props) {
   const { activities, totalCount } = await getActivities({
     method: 'offset',
-    size: 8,
+    size: 16,
     keyword: searchParams.keyword,
     page: searchParams.page ? searchParams.page : 1,
   });
 
   return (
-    <InnerLayout className="mb-[200px] mt-[40px] md:mb-[342px] md:mt-[60px]">
+    <InnerLayout className="mb-[200px] mt-[24px] md:mb-[342px] lg:mt-[40px]">
+      <div className="flex flex-col gap-[12px]">
+        <h2 className="text-24pxr font-[400] md:text-32pxr">
+          <b className="font-[700]">{searchParams.keyword}</b>(으)로 검색한 결과입니다.
+        </h2>
+        <data>총 {totalCount}개의 결과</data>
+      </div>
       <ActivityCardList activityList={activities} />
       {!totalCount ? (
         <div className="mt-[30px] text-center">게시물이 없습니다.</div>
