df8d98ab95dda70fdbab2215a7dda85c94b70426
ohdong9795 <ohdong9795@naver.com>
2024-07-23 14:42:52 +0900

    feat: Update layout file

--- src/app/(main)/(protected)/layout.tsx
+++ src/app/(main)/(protected)/layout.tsx
@@ -2,23 +2,20 @@ import InnerLayout from '@/components/atoms/inner-layout/InnerLayout';
 import SideNavigationMenu from '@/components/organisms/side-navigation-menu/SideNavigationMenu';
 import Footer from '@/components/templates/footer/Footer';
 import Header from '@/components/templates/header/Header';
-
 export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex h-screen flex-col">
+    <div className="flex min-h-screen flex-col bg-var-gray8">
       <Header />
-      <div className="flex flex-1 bg-var-gray8">
-        <InnerLayout>
-          <div className="flex py-[72px] md:gap-[16px] lg:gap-[24px]">
-            <SideNavigationMenu />
-            <div className="flex-1">{children}</div>
-          </div>
-        </InnerLayout>
-      </div>
+      <InnerLayout>
+        <div className="flex py-[72px] md:gap-[16px] lg:gap-[24px]">
+          <SideNavigationMenu />
+          <div className="w-full">{children}</div>
+        </div>
+      </InnerLayout>
       <Footer />
     </div>
   );
