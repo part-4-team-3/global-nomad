d01af1bdbc4e565a5fbbc49b09553fd41c335090
bk-git-hub <bkprivate99@gmail.com>
2024-07-25 15:17:37 +0900

    feat: 선택여부를 받는 프로퍼티 추가

--- src/components/atoms/schedule-button/ScheduleButton.tsx
+++ src/components/atoms/schedule-button/ScheduleButton.tsx
@@ -2,13 +2,17 @@ import { ButtonHTMLAttributes } from 'react';
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
+  isSelected?: boolean;
 }
 
-export default function ScheduleButton({ children, ...rest }: Props) {
+export default function ScheduleButton({ children, isSelected = false, ...rest }: Props) {
+  const style = isSelected ? 'bg-var-green-dark text-white' : '';
+
   return (
     <button
+      type="button"
       {...rest}
-      className="rounded-[8px] border border-var-green-dark px-12pxr py-10pxr font-[500] text-var-green-dark focus:bg-var-green-dark focus:text-white"
+      className={`rounded-[8px] border border-var-green-dark px-12pxr py-10pxr font-[500] text-var-green-dark ${style}`}
     >
       {children}
     </button>
