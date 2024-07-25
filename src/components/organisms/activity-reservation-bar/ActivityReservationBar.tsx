8ca5f30b1b1ad1f1923eddf01d58be58d1974eb1
bk-git-hub <bkprivate99@gmail.com>
2024-07-25 15:36:28 +0900

    refactor: Form을 이용하여 예약폼 리팩터링

--- src/components/organisms/activity-reservation-bar/ActivityReservationBar.tsx
+++ src/components/organisms/activity-reservation-bar/ActivityReservationBar.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState } from 'react';
+import { useForm, Controller } from 'react-hook-form';
 import Button from '@/components/atoms/button/Button';
 import { ActivityReservationSelector } from '@/components/molecules/activity-reservation-selector/ActivityReservationSelector';
 import { Schedule } from '@/types/schedule';
@@ -21,72 +22,89 @@ export default function ActivityReservationBar({
   const [isScheduleSelectorOpen, setIsScheduleSelectorOpen] = useState<boolean>(false);
   const [isParticipantSelectorOpen, setIsParticipantSelectorOpen] = useState<boolean>(false);
   const { selectedSchedule, setSelectedSchedule, participants, setParticipants } = useReservation();
-  const selectedId = selectedSchedule ? selectedSchedule.id : -1;
-  const dateButtonText = selectedSchedule
-    ? `${format(selectedSchedule.date, 'yy/MM/dd')} ${selectedSchedule.startTime} ~ ${selectedSchedule.endTime}`
-    : '날짜 선택하기';
 
-  const handleReservation = async (
-    activityId: number,
-    selectedScheduleId: number | undefined,
-    participants: number,
-  ) => {
-    const res = await postReservation(activityId, selectedScheduleId, participants);
+  const {
+    control,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      selectedSchedule: selectedSchedule,
+      participants: participants,
+    },
+  });
+
+  const handleReservation = async (data: {
+    selectedSchedule: Schedule | undefined;
+    participants: number;
+  }) => {
+    const { selectedSchedule, participants } = data;
+    const res = await postReservation(activityId, selectedSchedule?.id, participants);
     if (res < 0) {
       toast('예약에 실패했습니다');
       return;
     }
-
     toast('예약에 성공했습니다');
   };
 
+  const dateButtonText = selectedSchedule
+    ? `${format(selectedSchedule.date, 'yy/MM/dd')} ${selectedSchedule.startTime} ~ ${selectedSchedule.endTime}`
+    : '날짜 선택하기';
+
   return (
     <div className="fixed bottom-[0px] left-[0px] right-[0px] z-10 flex w-screen justify-between border-t border-[#a1a1a1] bg-white p-[16px] md:hidden lg:hidden">
-      <div className="flex flex-col gap-8pxr">
+      <form onSubmit={handleSubmit(handleReservation)} className="flex flex-col gap-8pxr">
         <div className="flex gap-6pxr font-[500]">
-          <PriceDisplay price={price * participants} />
+          <PriceDisplay price={price * watch('participants')} />
           <span className="text-20pxr font-[600]">/</span>
           <button
+            type="button"
             className="text-18pxr text-var-green-dark underline"
             onClick={() => setIsParticipantSelectorOpen(true)}
           >
-            {participants}명
+            {watch('participants')}명
           </button>
         </div>
         <button
+          type="button"
           className="w-fit text-14pxr font-[600] text-var-green-dark underline"
           onClick={() => setIsScheduleSelectorOpen(true)}
         >
           {dateButtonText}
         </button>
-      </div>
 
-      <Button
-        text="예약하기"
-        color="black"
-        className="px-24pxr py-14pxr disabled:bg-var-gray3"
-        disabled={!selectedSchedule}
-        onClick={() => {
-          handleReservation(activityId, selectedSchedule?.id, participants);
-        }}
-      ></Button>
+        <Button
+          text="예약하기"
+          color="black"
+          className="px-24pxr py-14pxr disabled:bg-var-gray3"
+          disabled={!watch('selectedSchedule')}
+        ></Button>
 
-      {isScheduleSelectorOpen && (
-        <ActivityReservationSelector
-          scheduleHash={scheduleHash}
-          scheduledDates={scheduledDates}
-          onClose={() => setIsScheduleSelectorOpen(false)}
-          onSelect={(schedule: Schedule | undefined) => setSelectedSchedule(schedule)}
-        />
-      )}
+        {isScheduleSelectorOpen && (
+          <ActivityReservationSelector
+            scheduleHash={scheduleHash}
+            scheduledDates={scheduledDates}
+            onClose={() => setIsScheduleSelectorOpen(false)}
+            onSelect={(schedule: Schedule | undefined) => {
+              setValue('selectedSchedule', schedule);
+              setSelectedSchedule(schedule);
+            }}
+          />
+        )}
 
-      {isParticipantSelectorOpen && (
-        <ActivityParticipantSelector
-          initialValue={participants}
-          onClose={() => setIsParticipantSelectorOpen(false)}
-          onSelect={(value) => setParticipants(value)}
-        />
-      )}
+        {isParticipantSelectorOpen && (
+          <ActivityParticipantSelector
+            initialValue={watch('participants')}
+            onClose={() => setIsParticipantSelectorOpen(false)}
+            onSelect={(value) => {
+              setValue('participants', value);
+              setParticipants(value);
+            }}
+          />
+        )}
+      </form>
     </div>
   );
 }
