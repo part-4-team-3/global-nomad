9b11f2cff2bb6c8307fcfc49597147f9ccb117d0
bk-git-hub <bkprivate99@gmail.com>
2024-07-29 10:56:45 +0900

    design: 체험 상세 설명에 whitespace pre line 적용

--- src/components/molecules/activity-description/ActivityDescription.tsx
+++ src/components/molecules/activity-description/ActivityDescription.tsx
@@ -6,7 +6,7 @@ export default function ActivityDescription({ description }: Props) {
   return (
     <div className="flex flex-col gap-[16px] px-[24px] py-[16px] md:border-t md:border-var-gray4 md:pb-[64px] md:pt-[40px]">
       <span className="text-20pxr font-[700]">체험 설명</span>
-      <p className="text-var-green-dark2">{description}</p>
+      <p className="whitespace-pre-line text-var-green-dark2">{description}</p>
     </div>
   );
 }
