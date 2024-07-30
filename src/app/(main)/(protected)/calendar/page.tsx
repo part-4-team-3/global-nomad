9c023511615457bbbde176a3cef8d0c6ae86dc3a
striker1826 <alstjq123579@daum.net>
2024-07-30 14:43:04 +0900

    refactor: 내 체험 현황 ssr --> csr로 변경

--- src/app/(main)/(protected)/calendar/page.tsx
+++ src/app/(main)/(protected)/calendar/page.tsx
@@ -1,71 +1,10 @@
 import MyReservation from '@/components/templates/mypage/MyReservation';
 import { getInstance } from '@/lib/axios';
-import { Activity } from '@/types/activity';
-import { isAxiosError } from 'axios';
-import { Metadata } from 'next';
-import { redirect } from 'next/navigation';
 
-export const metadata: Metadata = {
-  title: '예약 연황 | Global Nomad',
-  description: 'global nomad 내 체험 현황 페이지입니다.',
-};
-
-interface Props {
-  searchParams: { [key: string]: string | string[] | undefined };
-}
-
-export default async function page({ searchParams }: Props) {
-  let myReservationByMonth;
-  let selectedActivity;
-  let myActivtyListResponse;
-  const activityId = searchParams['activityId'];
-  let year = searchParams['year'];
-  let month = searchParams['month'];
-
+export default async function page() {
   const apiInstance = getInstance();
+  const myActivtyListResponse = await apiInstance.get(`my-activities`);
+  const myActivtyList = myActivtyListResponse.data;
 
-  myActivtyListResponse = await apiInstance.get(`my-activities`);
-
-  const myActivityList = myActivtyListResponse.data;
-
-  if (activityId) {
-    selectedActivity = myActivityList.activities.find(
-      (activity: Activity) => String(activity.id) === activityId,
-    );
-
-    if (!year || !month) {
-      const date = new Date();
-      const year = date?.getFullYear().toString();
-      const month = (date?.getMonth()! + 1).toString()!.padStart(2, '0');
-      const result = await apiInstance.get(
-        `my-activities/${activityId}?year=${year}&month=${month}`,
-      );
-      myReservationByMonth = result.data;
-    } else {
-      if (Number(month) < 0 || Number(month) > 12) {
-        const date = new Date();
-        year = date?.getFullYear().toString();
-        month = (date?.getMonth()! + 1).toString()!.padStart(2, '0');
-        redirect(`/calendar?activityId=${activityId}&year=${year}&month=${month}`);
-      }
-      month[0].padStart(2, '0');
-      try {
-        let result = await apiInstance.get(
-          `my-activities/${activityId}?year=${year}&month=${month}`,
-        );
-        myReservationByMonth = result.data;
-      } catch (err) {
-        redirect('/calendar');
-      }
-    }
-  }
-
-  return (
-    <MyReservation
-      myActivityList={myActivityList}
-      activityId={Number(activityId) || null}
-      selectedActivity={selectedActivity}
-      myReservationByMonth={myReservationByMonth}
-    />
-  );
+  return <MyReservation myActivityList={myActivtyList} />;
 }
