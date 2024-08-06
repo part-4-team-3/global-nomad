a6bbb08cd358cb26555f239a440e081b1422c837
daeun <qzsz797979@gmail.com>
2024-08-06 11:52:08 +0900

    design: 이메일 인증 ui 수정

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -14,14 +14,14 @@ export async function POST(req: NextRequest) {
       subject: 'GlobalNomad의 이메일 인증번호를 확인하세요',
       html: `
       <center style="font-family:pretendard">
-        <div style="display: flex; flex-direction: column; gap: 20px; justify-content: center;  align-items: center; background-color: #fafafa;">
+        <div style="display: flex; flex-direction: column; padding: 50px 0; gap: 20px; justify-content: center;  align-items: center; background-color: #fafafa;">
           <h1>
             <img src="https://glabal-nomad.vercel.app/logo-big.svg" style="width: 340px; max-width: 80%;" />
           </h1>
-          <div style="width: 500px; font-size: 20px; background-color: white; max-width: 90%; padding: 30px;">
-            <p>안녕하세요. 글로벌노마드에서 요청하신 인증번호를 보내드립니다.</p>
-            <data style="font-weight: 700; font-size: 25px; padding-y: 20px;">${code}</data>
-            <p>위 인증번호를 인증번호 입력창에 입력해 주세요.</p>
+          <div style="width: 500px; border-radius: 30px; font-size: 20px; background-color: white; max-width: 90%; padding: 30px;">
+            <p>글로벌노마드에서 요청하신 인증번호를 보내드립니다.</p>
+            <data style="font-weight: 700; font-size: 27px; padding: 20px 0;">${code}</data>
+            <p>위 인증번호를 입력해 주세요.</p>
           </div>
         </div>
       </center>`,
