e05319ff73896d23be8c8a78d75d2686b9fbc08c
daeun <qzsz797979@gmail.com>
2024-08-08 00:13:24 +0900

    refactor: 이메일인증 로고 패스 수정, 회원가입 ui 수정

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -8,8 +8,8 @@ export default function Layout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex h-svh w-full items-start justify-center">
-      <div className="mx-[12px] my-[44px] flex w-350pxr flex-col items-center justify-start gap-[24px] md:mt-[72px] md:w-640pxr md:gap-[32px] lg:my-[104px]">
+    <div className="flex w-full items-start justify-center">
+      <div className="mx-[12px] my-[44px] flex w-350pxr flex-col items-center justify-start gap-[24px] md:my-[72px] md:w-640pxr md:gap-[32px] lg:my-[104px]">
         <Link href="/">
           <Image
             className="mb-0 h-154pxr w-270pxr md:mb-[8px] md:h-192pxr md:w-340pxr"
